03ad5d05544f9cbffac62abb1294c1cdf179e74e
israaHamdi <israaaburayya2012@gmail.com>
2022-08-04 09:56:38 +0200

    add route for handle set headers

--- server/routes/index.js
+++ server/routes/index.js
@@ -19,7 +19,13 @@ import {
 import { verifyToken, verifyTokenAndAuthorization } from '../middleware/index.js';
 
 const router = Router();
-
+router.get('/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Max-Age', '1800');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS');
+});
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.get('/categories', getCategoriesController);
