fad2f4abc81250ac51bf50811e3ac2e18b82d627
israaHamdi <israaaburayya2012@gmail.com>
2022-08-07 18:21:41 +0200

    edit handle review

--- server/controllers/products/getSingleProductReviewController.js
+++ server/controllers/products/getSingleProductReviewController.js
@@ -2,8 +2,9 @@ import { getSingleProductReviewByIdQuery } from '../../database/queries/index.js
 
 const getSingleProductReviewController = async (req, res, next) => {
   try {
+    const { page } = req.query;
     const { productId } = req.params;
-    const { rows } = await getSingleProductReviewByIdQuery(productId);
+    const { rows } = await getSingleProductReviewByIdQuery(productId, page);
     res.json({ message: 'Successfully retrieved product reviews ', status: 200, data: rows });
   } catch (error) {
     next(error);
--- server/database/queries/products/getSingleProductReviewByIdQuery.js
+++ server/database/queries/products/getSingleProductReviewByIdQuery.js
@@ -1,10 +1,10 @@
 import connection from '../../config/connection.js';
 
-const getSingleProductReviewByIdQuery = (productId) => {
+const getSingleProductReviewByIdQuery = (productId, page) => {
   const sql = {
-    text: `SELECT reviews.comment,reviews.rate,users.name FROM reviews 
-     JOIN users ON reviews.user_id = users.id  GROUP BY reviews.id,users.id HAVING product_id=$1 ORDER BY reviews.createdAt DESC `,
-    values: [productId],
+    text: `SELECT reviews.comment,reviews.rate,users.name, reviews.createdAt FROM reviews 
+     JOIN users ON reviews.user_id = users.id  GROUP BY reviews.id,users.id HAVING product_id=$1 ORDER BY reviews.createdAt DESC LIMIT 3 OFFSET $2*3 `,
+    values: [productId, page],
   };
   return connection.query(sql);
 };
