98a1f16c7de26ddaca8546553574c4781dfc4d7e
israaHamdi <israaaburayya2012@gmail.com>
2022-08-15 22:30:41 +0200

    edit routes

--- server/routes/wishlist.js
+++ server/routes/wishlist.js
@@ -7,13 +7,6 @@ import {
 import { verifyTokenAndAuthorization } from '../middleware/index.js';
 
 const router = Router();
-router.get('/', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Max-Age', '1800');
-  res.setHeader('Access-Control-Allow-Headers', 'content-type');
-  res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS');
-});
 router.route('/user/:id/wishlist').get(verifyTokenAndAuthorization, getUserWishlistsController)
   .post(verifyTokenAndAuthorization, addProductsToUserWishlistsController);
 router.delete('/user/:id/wishlist/:productId', verifyTokenAndAuthorization, deleteProductFromUserWishlistsController);
