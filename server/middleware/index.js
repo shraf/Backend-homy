66ccfa85f080184e4183a09f34f62b47e7438007
israaHamdi <israaaburayya2012@gmail.com>
2022-08-18 13:52:26 +0200

    handle link for page

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -35,7 +35,7 @@ const verifyTokenAndAdminAuthorization = (req, res, next) => {
         return next();
       }
       if (req.user.role != 1 && req.user.role != 2) {
-        const pageLink = req.headers && req.headers.referer && req.headers.referer.split('/')[3];
+        const pageLink = req.headers && req.headers.referer && req.headers.referer.split('/')[4];
         if (data.length) {
           const x = data.find((item) => item.methodname.toLowerCase() === req.method.toLowerCase());
           if (x?.pagename == pageLink) {
@@ -56,18 +56,3 @@ export {
   verifyTokenAndAuthorization,
   verifyTokenAndAdminAuthorization,
 };
-
-//data.find((item) => item.methodname.toLowerCase() === 'post' && item.pagename.toLowerCase == 'products'); &&
-// &&<Link to = ' '>employees</Link>
-
-
-
-
-
-// for categories
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiZW1haWwiOiJlbXBsb3llZTRAZ21haWwuY29tIiwibmFtZSI6ImVtcGxveWVlMSIsInBob25lIjoiNjQ4NTY0ODY4NDU2ODQ2Iiwicm9sZSI6NCwiaWF0IjoxNjYwODA2NjU0fQ.PA-sZBbbFkX0ynaoSUJ2QfCiBRzUTeJjiLdB9hQ0b3k
-
-// for admin
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJuYW1lIjoiYWRtaW4iLCJwaG9uZSI6Iis5NjUxMjM0NTY3OCIsInJvbGUiOjIsImlhdCI6MTY2MDc2NzExOH0.pJOscYqzkl2lVMUIa6dcpEAdn5956q5It9EpSL355QE
-
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiZW1haWwiOiJlbXBsb3llZTFAZ21haWwuY29tIiwibmFtZSI6ImVtcGxveWVlMSIsInBob25lIjoiKzk2NTEyMzQ1Njc4Iiwicm9sZSI6MywiaWF0IjoxNjYwNzY0NDkwfQ.E7Sp_31rQcFHByImHTTIG8CCiUk0-x9D543fO47rvME
