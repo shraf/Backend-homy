d0cc4bbc3720348a95ce63bb1107d00bf5489f25
israaHamdi <israaaburayya2012@gmail.com>
2022-08-20 23:31:03 +0200

    edit user query

--- server/database/queries/users/getUsersAndEmployeesQuery.js
+++ server/database/queries/users/getUsersAndEmployeesQuery.js
@@ -2,7 +2,7 @@ import connection from '../../config/connection.js';
 
 const getUsersAndEmployeesQuery = () => {
   const sql = {
-    text: 'SELECT * FROM users JOIN roles ON users.role_id = roles.id WHERE users.role_id != 2',
+    text: 'SELECT u.id as userId, r.id as roleId,u.email,u.phone,u.name,r.role FROM users as u JOIN roles as r ON u.role_id = r.id WHERE u.role_id != 2',
     values: [],
   };
   return connection.query(sql);
