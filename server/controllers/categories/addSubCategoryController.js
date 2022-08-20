4c2b69c888d0f48a58c60e18acfd98c0ea73bfaf
israaHamdi <israaaburayya2012@gmail.com>
2022-08-21 00:20:14 +0200

    edit update and add for sub categories

--- server/controllers/categories/addSubCategoryController.js
+++ server/controllers/categories/addSubCategoryController.js
@@ -1,18 +1,8 @@
-import { addCategoryQuery } from '../../database/queries/index.js';
 import { customizedError } from '../../utils/index.js';
-import categorySchema from '../../utils/validation/categorySchema.js';
 
 const addSubCategoryController = async (req, res, next) => {
   try {
-    const {
-      name, image, place, hasSubCategories,
-    } = await categorySchema.validate(req.body, { abortEarly: false });
-    const { rows: data } = await addCategoryQuery(name, image, place, hasSubCategories);
-    res.status(201).json({
-      message: 'You have been successfully added category',
-      status: 201,
-      data,
-    });
+
   } catch (error) {
     if (error.name === 'ValidationError') {
       return next(customizedError(400, error.errors[0]));
--- server/controllers/categories/updateSubCategoryController.js
+++ server/controllers/categories/updateSubCategoryController.js
@@ -1,19 +1,10 @@
-import { addCategoryQuery, updateCategoryQuery } from '../../database/queries/index.js';
+
 import { customizedError } from '../../utils/index.js';
-import categorySchema from '../../utils/validation/categorySchema.js';
 
 const updateSubCategoryController = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const {
-      name, image, place, hasSubCategories,
-    } = await categorySchema.validate(req.body, { abortEarly: false });
-    const { rows: data } = await updateCategoryQuery(name, image, place, hasSubCategories, id);
-    res.json({
-      message: 'You have been successfully updated category',
-      status: 200,
-      data,
-    });
+
   } catch (error) {
     if (error.name === 'ValidationError') {
       return next(customizedError(400, error.errors[0]));
