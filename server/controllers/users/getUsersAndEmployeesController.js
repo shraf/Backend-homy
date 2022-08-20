3aadf6e7d57c37463c6dd9f740eeb894b74d4f4b
israaHamdi <israaaburayya2012@gmail.com>
2022-08-20 22:19:40 +0200

    edit message

--- server/controllers/users/getUsersAndEmployeesController.js
+++ server/controllers/users/getUsersAndEmployeesController.js
@@ -3,7 +3,7 @@ import { getUsersAndEmployeesQuery } from '../../database/queries/index.js';
 const getUsersAndEmployeesController = async (req, res, next) => {
   try {
     const { rows } = await getUsersAndEmployeesQuery();
-    res.json({ message: 'Successfully retrieved All Permissions ', status: 200, data: rows });
+    res.json({ message: 'Successfully retrieved All users ', status: 200, data: rows });
   } catch (error) {
     next(error);
   }
